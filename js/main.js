e6a1392e854f43d1f0b2dbd0005250cb6d253dc4
cochy98 <cosmoferrigno@gmail.com>
2022-02-23 23:33:36 +0100

    aggiunto reset input da javascript

--- js/main.js
+++ js/main.js
@@ -9,20 +9,20 @@
 */
 
 // Vado a leggere lo stato del button
-let userButton = document.getElementById('btn-success');
+const userButton = document.getElementById('btn-success');
 
 // Richiamo una funzione anonima quando clicco sul button di invio
 userButton.addEventListener('click', function(){
     
     // In queste variabili vado ad immagazzinare i dati degli input form
-    let userNamePassenger = document.getElementById('name-passenger').value;
-    let numberKilometres = document.getElementById('number-km').value;
-    let etaPassenger = document.getElementById('eta-passenger').value;
+    let userNamePassenger = document.getElementById('name-passenger');
+    let numberKilometres = document.getElementById('number-km');
+    let etaPassenger = document.getElementById('eta-passenger');
 
     // Costante costo al km
     const priceKm = 0.27;
     // Calcolo costo del biglietto
-    let priceTicket = priceKm * numberKilometres;
+    let priceTicket = priceKm * numberKilometres.value;
 
     let message = "";
 
@@ -31,23 +31,27 @@ userButton.addEventListener('click', function(){
     {
         priceTicket -= priceTicket * 0.17; 
         priceTicket = priceTicket.toFixed(2); // Converto il numero con sole 2 cifre decimali
-        message = `${userNamePassenger}, con i suoi ${etaPassenger} anni è minorenne, ha diritto ad uno sconto del 17%. Pagherà in totale ${priceTicket}€, per ${numberKilometres}.`;
+        message = `${userNamePassenger.value}, con i suoi ${etaPassenger.value} anni è minorenne, ha diritto ad uno sconto del 17%. Pagherà in totale ${priceTicket}€, per ${numberKilometres}.`;
     } 
 
     // Altrimenti se over 65, applico uno sconto del 33% e mostro il risultato con solo 2 cifre decimali
     else if (etaPassenger == "over65"){
         priceTicket -= priceTicket * 0.33;
         priceTicket = priceTicket.toFixed(2);
-        message = `${userNamePassenger}, con i suoi ${etaPassenger} anni è un Over 65, ha diritto ad uno sconto del 40%. Pagherà in totale ${priceTicket}€, per ${numberKilometres}.`;
+        message = `${userNamePassenger.value}, con i suoi ${etaPassenger.value} anni è un Over 65, ha diritto ad uno sconto del 40%. Pagherà in totale ${priceTicket}€, per ${numberKilometres}.`;
     } 
 
     // Visualizzo in console
     console.log(message);
 
     // Visualizzo nel body
-    document.getElementById('user-name').innerHTML = userNamePassenger;
+    document.getElementById('user-name').innerHTML = userNamePassenger.value;
     document.getElementById('offer').innerHTML = "Biglietto Standard";
     document.getElementById('carrozza').innerHTML = "5";
     document.getElementById('cod').innerHTML = "92911";
-    document.getElementById('price').innerHTML = priceTicket;
+    document.getElementById('price').innerHTML = priceTicket.value;
+
+    // Resetto gli input
+    userNamePassenger.value = "";
+    numberKilometres.value = "";
 });
